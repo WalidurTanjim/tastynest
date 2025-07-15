d477406113eb22c6045abf889b17afe02b1fe5f3
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-15 16:13:43 +0600

    TastyNest setup and Navbar created

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import MainLayout from './layout/MainLayout'
+import Home from './pages/Home/Home'
+
+function App() {
+  const router = createBrowserRouter([
+    {path: '/', element: <MainLayout />, children: [
+      {path: '/', element: <Home />}
+    ]}
+  ])
+
+  return <RouterProvider router={router} />
+}
+
+export default App
