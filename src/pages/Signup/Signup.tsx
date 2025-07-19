b2d4bc832f10dc11ac8217ec9988e2839354da66
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-19 15:22:45 +0600

    react-hook-form install & used on the Signup.tsx page & validate password with passRegEx

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -2,13 +2,40 @@ import { useState } from "react";
 import './Signup.css';
 import { Link } from "react-router-dom";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+// react-hook-form input type
+type Inputs = {
+  fullname: string;
+  email: string;
+  createPassword: string;
+  repeatPassword: string;
+  phone: string;
+}
 
 const Signup = () => {
+    // states
     const [showPassword, setShowPassword] = useState<boolean>(false);
+    const passRegEx = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z]).{6,}$/;
 
     // showPasswordHandler
     const showPasswordHandler = () => {
         setShowPassword(!showPassword);
+    }
+
+
+    // react-hook-form
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        // password validation
+        if(!passRegEx.test(data?.createPassword)){
+            return alert("Password must be follow the rules");
+        }
+        if(data?.repeatPassword !== data?.createPassword){
+            return alert("Both are not same");
+        }
+
+        console.log(data);
     }
 
     return (
@@ -16,30 +43,35 @@ const Signup = () => {
             <div className="container mx-auto w-full sm:w-4/5 md:w-4/6 lg:w-2/6 px-2 sm:px-6 lg:px-8 py-10">
                 <SectionTitle title="Signup Here" />
 
-                <form className="w-full mt-10">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-full mt-10">
                     <div className="fullname w-full mb-4">
                         <label htmlFor="fullname">Fullname</label>
-                        <input type="text" id="fullname" placeholder="Fullname" autoComplete="off" required className="custom-input"  />
+                        <input type="text" id="fullname" placeholder="Fullname" autoComplete="off" required className="custom-input" {...register("fullname", { required: true })}  />
+                        {errors.fullname && <span className="text-sm text-red-600">This field is required</span>}
                     </div>
 
                     <div className="email w-full mb-4">
                         <label htmlFor="email">Email address</label>
-                        <input type="email" id="email" placeholder="Email address" autoComplete="off" required className="custom-input"  />
+                        <input type="email" id="email" placeholder="Email address" autoComplete="off" required className="custom-input" {...register("email", { required: true })}  />
+                        {errors.email && <span className="text-sm text-red-600">This field is required</span>}
                     </div>
 
                     <div className="createPassword w-full mb-4">
                         <label htmlFor="createPassword">Create Password</label>
-                        <input type={showPassword ? 'text' : 'password'} id="createPassword" placeholder="Create Password" autoComplete="off" required className="custom-input"  />
+                        <input type={showPassword ? 'text' : 'password'} id="createPassword" placeholder="Create Password" autoComplete="off" required className="custom-input" {...register("createPassword", { required: true })}  />
+                        {errors.createPassword && <span className="text-sm text-red-600">This field is required</span>}
                     </div>
 
                     <div className="repeatPassword w-full mb-4">
                         <label htmlFor="repeatPassword">Repeat Password</label>
-                        <input type={showPassword ? 'text' : 'password'} id="repeatPassword" placeholder="Repeat Password" autoComplete="off" required className="custom-input"  />
+                        <input type={showPassword ? 'text' : 'password'} id="repeatPassword" placeholder="Repeat Password" autoComplete="off" required className="custom-input" {...register("repeatPassword", { required: true })}  />
+                        {errors.repeatPassword && <span className="text-sm text-red-600">This field is required</span>}
                     </div>
 
                     <div className="phone w-full mb-4">
                         <label htmlFor="phone">Phone number</label>
-                        <input type="number" id="phone" placeholder="Phone number" autoComplete="off" required className="custom-input"  />
+                        <input type="number" id="phone" placeholder="Phone number" autoComplete="off" required className="custom-input" {...register("phone", { required: true })}  />
+                        {errors.phone && <span className="text-sm text-red-600">This field is required</span>}
                     </div>
 
                     <div className="mb-4 flex gap-2 text-sm">
